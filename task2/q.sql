b8c2b78254888668622b9efc6e8caef685c38f4c
aitkachuk <user7@tc.geeksforless.net>
2015-02-17 11:14:21 +0200

    task2

---
+++ task2/q.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS test2;
+
+ DELIMITER //
+create procedure fun1() 
+begin
+    set @p := 0;
+    while @p<=700000 do 
+        set @p:=@p+1;
+        set @t:= HEX(FLOOR(RAND() * 25600000));
+        INSERT INTO test2( num, myvar ,mytext) VALUES (@p , 
+            @t , md5(@t) );
+
+    end while;
+
+end;
+
+
+ 
+
+
+
+
+
+
+
+ //
+  DELIMITER ;
+    CREATE TABLE test2 (num INT, myvar VARCHAR(255), mytext TEXT);
+    call fun1();
+    drop procedure  IF EXISTS fun1;
+    
+    ALTER TABLE test2 ADD FULLTEXT  (mytext);
+
+ /*   SELECT num FROM test2;*/
