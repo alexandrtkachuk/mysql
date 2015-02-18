d04592ab30a0a0588e191bf6ce393fab1ed09202
aitkachuk <user7@tc.geeksforless.net>
2015-02-18 09:39:12 +0200

    task 3

---
+++ task3/q.sql
@@ -0,0 +1,58 @@
+DROP TABLE IF EXISTS task3_TABLE_A ;
+DROP TABLE IF EXISTS task3_TABLE_B ;
+
+CREATE TABLE task3_TABLE_A (id INT, name VARCHAR(255));
+CREATE TABLE task3_TABLE_B (id INT, name VARCHAR(255));
+
+INSERT INTO task3_TABLE_A   (id, name) VALUE(1 ,'Pirate' );
+INSERT INTO task3_TABLE_A   (id, name) VALUE(2 ,'Monkey' );
+INSERT INTO task3_TABLE_A   (id, name) VALUE(3 ,'Ninja' );
+INSERT INTO task3_TABLE_A   (id, name) VALUE(4 ,'Spaghetti' );
+
+
+INSERT INTO task3_TABLE_B   (id, name) VALUE(1 ,'Rutabaga' );
+INSERT INTO task3_TABLE_B   (id, name) VALUE(2 ,'Pirate' );
+INSERT INTO task3_TABLE_B   (id, name) VALUE(3 ,'Darth Vader');
+INSERT INTO task3_TABLE_B   (id, name) VALUE(4 ,'Ninja' );
+
+
+
+SELECT * FROM task3_TABLE_A
+INNER JOIN task3_TABLE_B
+ON task3_TABLE_A.name = task3_TABLE_B.name;
+
+
+
+
+SELECT * FROM task3_TABLE_A LEFT JOIN task3_TABLE_B 
+ON  task3_TABLE_A.name = task3_TABLE_B.name
+UNION 
+SELECT * FROM task3_TABLE_A RIGHT JOIN task3_TABLE_B 
+ON  task3_TABLE_A.name = task3_TABLE_B.name;
+
+
+
+SELECT * FROM task3_TABLE_A A
+LEFT OUTER JOIN task3_TABLE_B B
+ON A.name = B.name;
+
+
+SELECT * FROM task3_TABLE_A A
+LEFT OUTER JOIN task3_TABLE_B B 
+ON A.name = B.name
+WHERE B.id IS null;
+
+
+
+SELECT * FROM task3_TABLE_A A
+LEFT OUTER JOIN task3_TABLE_B B 
+ON A.name = B.name
+WHERE A.id IS null
+UNION
+SELECT * FROM task3_TABLE_A A
+RIGHT OUTER JOIN task3_TABLE_B B 
+ON A.name = B.name
+WHERE B.id IS null;
+
+
+
