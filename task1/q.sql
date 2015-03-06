ce65cdbafba4f8e2de8f97efd8de2e5e169573d2
aitkachuk <user7@tc.geeksforless.net>
2015-03-06 08:14:41 +0200

    end tasks

---
+++ task1/q.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS shop_books ;
+CREATE TABLE shop_books (id INT NOT NULL AUTO_INCREMENT , price FLOAT ,  
+    PRIMARY KEY (id) );
+
+DROP TABLE IF EXISTS shop_users;
+CREATE TABLE shop_users (id INT NOT NULL AUTO_INCREMENT ,
+    name VARCHAR(32),
+    pass VARCHAR(128),
+    email VARCHAR(64),
+    PRIMARY KEY (id) );
+
+DROP TABLE IF EXISTS shop_book2user;
+CREATE TABLE shop_book2user (idBook INT , idUser INT , count INT);
+
+DROP TABLE IF EXISTS shop_order2book;
+CREATE TABLE shop_order2book (idOrder INT , idBook INT , count INT , price FLOAT);
+
+DROP TABLE IF EXISTS shop_orders ;
+CREATE TABLE shop_orders (id INT NOT NULL AUTO_INCREMENT ,
+        idUser INT ,
+        oderDate DATETIME,  
+        PRIMARY KEY (id) );
+
+
+
+
